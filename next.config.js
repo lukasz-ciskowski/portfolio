ff416f23affda2a0be3cc1431f0195c093cd90e1
lukasz_ciskowski <lukasz_ciskowski@wp.pl>
2021-11-14 12:35:26 +0100

    Change server method

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    async redirects() {
+        console.log(process.env.PROXY_URL)
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${process.env.PROXY_URL}/api/:path*`,
+                permanent: true,
+            },
+        ]
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
