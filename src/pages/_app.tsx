47a648ebd1496ac4766280460a3fa558eb10b675
lukasz_ciskowski <lukasz_ciskowski@wp.pl>
2021-11-14 10:54:43 +0100

    Support server side styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from "next/app"
+import { GlobalStyle } from "style/GlobalStyle"
+import { theme } from "theme/theme"
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/styles"
+import { muiTheme } from "theme/muiTheme"
+import { ThemeProvider } from "styled-components"
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <MuiThemeProvider theme={muiTheme}>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </MuiThemeProvider>
+    )
 }
 export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import type { NextPage } from "next"
 import { useEffect } from "react"
-import { ThemeProvider } from "styled-components"
 import Layout from "components/Layout/Layout"
 import AboutSection from "sections/AboutSection/AboutSection"
 import HomeSection from "sections/HomeSection/HomeSection"
-import { GlobalStyle } from "style/GlobalStyle"
-import { theme } from "theme/theme"
 import AOS from "aos"
 import ExperienceSection from "sections/ExperienceSection/ExperienceSection"
 import ProjectsSection from "sections/ProjectsSection/ProjectsSection"
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/styles"
-import { muiTheme } from "theme/muiTheme"
 
 import "aos/dist/aos.css"
 import ContactSection from "sections/ContactSection/ContactSection"
@@ -21,18 +16,13 @@ const Home: NextPage = () => {
     }, [])
 
     return (
-        <MuiThemeProvider theme={muiTheme}>
-            <ThemeProvider theme={theme}>
-                <GlobalStyle />
-                <Layout>
-                    <HomeSection />
-                    <AboutSection />
-                    <ExperienceSection />
-                    <ProjectsSection />
-                    <ContactSection />
-                </Layout>
-            </ThemeProvider>
-        </MuiThemeProvider>
+        <Layout>
+            <HomeSection />
+            <AboutSection />
+            <ExperienceSection />
+            <ProjectsSection />
+            <ContactSection />
+        </Layout>
     )
 }
 
